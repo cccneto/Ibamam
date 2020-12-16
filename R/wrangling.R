d48205065d329836b07c330231309b431db13f70
Claudiano Neto <cneto@ufrb.edu.br>
2020-12-16 18:24:12 -0300

    ajustes na funcao conversao de moedas

--- R/wrangling.R
+++ R/wrangling.R
@@ -24,24 +24,4 @@ library(tidyverse)
 library(dplyr)
 
 
-df <- df %>%
-  dplyr::mutate(dataAuto = lubridate::dmy(dataAuto)) %>%
-  mutate(dataPagamento = lubridate::dmy(dataPagamento)) %>%
-  mutate(tipoInfracao = as.factor(tipoInfracao)) %>%
-  mutate(uf = as.factor(uf)) %>%
-  mutate(tipoAuto = as.factor(tipoAuto)) %>%
-  mutate(moeda = as.factor(moeda)) %>%
-  mutate(statusDebito = as.factor(statusDebito)) %>%
-  mutate(enquadramentoLegal = as.factor(enquadramentoLegal)) %>%
-  mutate(across(where(is.character), str_remove_all, pattern = fixed("  "))) %>%
-  mutate(enquadramentoJuridico = as.factor(if_else(nchar(cpfCnpj) <= 14,"CPF","CNPJ")))
-
-df %>% select(dataAutor)
-
-library(ggplot2)
-df %>%
-  select(moeda, dataAuto, dataPagamento, valorPago, valorAuto) %>%
-  group_by(moeda) %>%
-  arrange(moeda) %>%
-  ggplot() + geom_line(aes(valorPago, dataPagamento))
 
---
+++ sugestions.R
@@ -0,0 +1,12 @@
+################################
+# Ideias para trabalhar
+################################
+
+# http://www.igf.com.br/calculadoras/conversor_1.aspx
+# http://idealsoftwares.com.br/tabelas/tabela.php?id=351
+#
+# 1.	Explicar diferença das moedas
+# 2.	Mapa de distribuição das multas
+# 3.	Mapa de volume de recursos arrecadados
+# 4.	Mapa de volume de recursos não arrecadados??!!
+# 5.	Visualizar diferença de tempo nos pagamentos das multas
