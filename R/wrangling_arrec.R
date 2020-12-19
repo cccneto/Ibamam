04a818d4fa696756b0fadbd15d6ef6b7d257be25
Claudiano Neto <cneto@ufrb.edu.br>
2020-12-19 16:21:38 -0300

    ajuste do script

---
+++ R/wrangling_arrec.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(dplyr)
+
+# ajustando variaveis de Data para o formato correto
+
+# carregando dataframe
+load("df_arrecadacao.rda")
+
+# corrigindo classes das variaveis
+df_arrec <- df_arrecadacao %>%
+  mutate(dataAuto = lubridate::dmy(dataAuto)) %>%
+  mutate(dataPagamento = lubridate::dmy(dataPagamento)) %>%
+  mutate(tipoInfracao = as.factor(tipoInfracao)) %>%
+  mutate(uf = as.factor(uf)) %>%
+  mutate(tipoAuto = as.factor(tipoAuto)) %>%
+  mutate(moeda = as.factor(moeda)) %>%
+  mutate(statusDebito = as.factor(statusDebito)) %>%
+  mutate(enquadramentoLegal = as.factor(enquadramentoLegal)) %>%
+  mutate(across(where(is.character), str_remove_all, pattern = fixed("  "))) %>%
+  mutate(enquadramentoJuridico = as.factor(if_else(nchar(cpfCnpj) <= 14,"CPF","CNPJ")))
+
+df_arrec %>% glimpse()
