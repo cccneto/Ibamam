0243c7c21dc0984c464b3b3d8ddfa34fbc2f15f9
Claudiano Neto <cneto@ufrb.edu.br>
2020-12-23 18:50:33 -0300

    ajustando funcao conversao

--- R/converter.R
+++ R/converter.R
@@ -1,7 +1,7 @@
 # Criar a funcionalidade para atualizacao dos valores
 
-library(deflateBR)
 library(dplyr)
+li
 
 load("df_arrec.rda")
 
@@ -9,34 +9,49 @@ load("df_arrec.rda")
 
 converter <- function(moeda, valor, x = 1000, y= 2.75, z=1.0641){
   if(moeda == "UFIR"){
-valor * z # Valor da ufir em 12/2020
-  } else if (moeda == "OTN"){
-  ((valor * 6.17 * 126.8621)/597.06) * z
-  } else if (moeda == "BTN"){
-    ((valor * 126.8621)/597.06) * z
-  } else if (moeda == "MVR"){
-     valor * 63.49 # Valor da MVR em reais
-  } else if(moeda == "Cruzeiro (70 a 86)"){
-     valor / y * x^4
-  } else if (moeda == "Cruzado"){
-     valor / y * x^3
-  } else if (moeda == "Cruzado Novo"){
-     valor / y * x^2
-  } else if (moeda == "Cruzeiro (90 a 93)"){
-   valor / y * x^2
-  } else if(moeda == "Cruzeiro Real"){
-     valor / y * x
-  } else if (moeda == "Real"){
-    valor
+    new_valor <- valor * z # Valor da ufir em 12/2020
+  } dplyr::if_else(moeda == "OTN"){
+  new_valor <- ((valor * 6.17 * 126.8621)/597.06) * z
+  } dplyr::if_else(moeda == "BTN"){
+    new_valor <- ((valor * 126.8621)/597.06) * z
+  } dplyr::if_else(moeda == "MVR"){
+    new_valor <- valor * 63.49 # Valor da MVR em reais
+  }dplyr::if_else(moeda == "Cruzeiro (70 a 86)"){
+    new_valor <- valor / y * x^4
+  } dplyr::if_else(moeda == "Cruzado"){
+    new_valor <-  valor / y * x^3
+  }dplyr::if_else(moeda == "Cruzado Novo"){
+    new_valor <- valor / y * x^2
+  }dplyr::if_else(moeda == "Cruzeiro (90 a 93)"){
+    new_valor <- valor / y * x^2
+  } dplyr::if_else(moeda == "Cruzeiro Real"){
+    new_valor <-  valor / y * x
+  } dplyr::if_else(moeda == "Real"){
+    new_valor <- valor
   }
-  df <- tibble(df_arrec$valorAuto, valor)
-  return(df)
-}
+  }
+  # df <- tibble(df_arrec$valorAuto, valor, moeda)
+  # return(df)
+
 
 # PRECISA CONFERIR ESSA FUNÇÃO ACIMA
 teste <- converter(moeda = df_arrec$moeda, df_arrec$valorAuto)
 
+# dplyr::if_else
+# new_valor <- valor ...elt()
 
-teste %>%
-  mutate(soma = df_arrec$valorAuto - valor) %>%
-  View()
+converter <-
+  function(moeda, valor, x = 1000, y= 2.75, z=1.0641){
+    dplyr::if_else(moeda == "UFIR", new_valor <- valor * z, valor)
+    dplyr::if_else(moeda == "OTN", new_valor <- ((valor * 6.17 * 126.8621)/597.06) * z, valor)
+    dplyr::if_else(moeda == "BTN", new_valor <- ((valor * 126.8621)/597.06) * z, valor)
+    dplyr::if_else(moeda == "MVR", new_valor <- valor * 63.49, valor) # Valor da MVR em reais
+    dplyr::if_else(moeda == "Cruzeiro (70 a 86)", new_valor <- valor / y * x^4, valor)
+    dplyr::if_else(moeda == "Cruzado", new_valor <-  valor / y * x^3, valor)
+    dplyr::if_else(moeda == "Cruzado Novo", new_valor <- valor / y * x^2, valor)
+    dplyr::if_else(moeda == "Cruzeiro (90 a 93)", new_valor <- valor / y * x^2, valor)
+    dplyr::if_else(moeda == "Cruzeiro Real", new_valor <-  valor / y * x, valor)
+    dplyr::if_else(moeda == "Real", new_valor <- valor)
+  }
+
+testar <- converter(moeda = df_arrec$moeda, valor = df_arrec$valorAuto)
