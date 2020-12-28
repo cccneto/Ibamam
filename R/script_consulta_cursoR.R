038555cc79710a9018304650fe43740c09d8134d
Claudiano Neto <cneto@ufrb.edu.br>
2020-12-28 19:08:43 -0300

    inserindo dados para fazer um exemplo reproduzivel.

--- R/script_consulta_cursoR.R
+++ R/script_consulta_cursoR.R
@@ -2,13 +2,22 @@ library(dplyr)
 
 load("df_arrec.rda")
 
+# Se quiser simular dados para teste
+
+set.seed(100)
+moeda <- sample(c("Real", "Cruzado", "UFIR"), replace = TRUE, size = 100)
+valorAuto <- as.integer(sample(c(100:1000), replace = TRUE, size = 100))
+df_teste <- cbind.data.frame(moeda, valorAuto)
+
+# função
+
 converter <- function(moeda, valor, x = 1000, y= 2.75, z=1.0641){
   if(moeda == "UFIR"){
     new_valor <- valor * z # Valor da ufir em 12/2020
   } else if (moeda == "OTN"){
-    (( new_valor <-valor * 6.17 * 126.8621)/597.06) * z
+     new_valor <- (valor * 6.17 * 126.8621)/597.06 * z
   } else if (moeda == "BTN"){
-    ((new_valor <- valor * 126.8621)/597.06) * z
+    new_valor <- valor * 126.8621/597.06 * z
   } else if (moeda == "MVR"){
     new_valor <- valor * 63.49 # Valor da MVR em reais
   } else if(moeda == "Cruzeiro (70 a 86)"){
@@ -24,12 +33,17 @@ converter <- function(moeda, valor, x = 1000, y= 2.75, z=1.0641){
   } else if (moeda == "Real"){
     new_valor <- valor
   }
-  df <- tibble(df_arrec$valorAuto, valor, moeda, new_valor)
+  df <- tibble(valor, moeda, new_valor)
   return(df)
 }
 
 # atribuindo objeto para conferir resultado
-teste <- converter(moeda = df_arrec$moeda, df_arrec$valorAuto)
+teste <- converter(moeda = df_arrec$moeda, valor = df_arrec$valorAuto)
 
 # conferindo resultado da funcao
 teste %>% View()
+
+
+# atribuindo objeto para conferir resultado
+teste <- converter(moeda = df_teste$moeda, valor = df_teste$valorAuto)
+
