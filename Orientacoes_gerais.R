23e24ffef6a944638fedf304ee4fa0f2eaa6d039
Claudiano Neto <cneto@ufrb.edu.br>
2021-01-11 09:48:25 -0300

    passos a seguir.

---
+++ Orientacoes_gerais.R
@@ -0,0 +1,8 @@
+# Fluxo do trabalho realizado ate aqui
+
+# 1 - raspagens
+# 2 - Wrangling
+# 3 - puxar id municipal
+# 4 - realizar função para mudar moedas
+# 5 - realizar funcao para converter valores
+
