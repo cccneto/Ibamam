91d4d95bdde1d089bf7995139784a45100b601bd
Claudiano Neto <cneto@ufrb.edu.br>
2021-02-03 20:33:44 -0300

    ajustes gerais

---
+++ R/id_muni.R
@@ -0,0 +1,8 @@
+#' @title Inserir codigos municipais dos municipios
+#'
+#' @param ufs bla bla bla
+#' @param path Path where the jason files will be saved.
+#'
+#' @return a data frame
+#'
+#' @export
