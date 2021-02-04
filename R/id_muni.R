0287c56fc200d9f3c97231e9ba9a5ac8272dc5ee
Claudiano Neto <cneto@ufrb.edu.br>
2021-02-04 13:33:01 -0300

    criando funcao baixar_id ibge

--- R/id_muni.R
+++
@@ -1,8 +0,0 @@
-#' @title Inserir codigos municipais dos municipios
-#'
-#' @param ufs bla bla bla
-#' @param path Path where the jason files will be saved.
-#'
-#' @return a data frame
-#'
-#' @export
--- R/juntar_bases.R
+++ R/juntar_bases.R
@@ -1,4 +1,4 @@
-#' @title Baixar codigos municipais do IBGE
+#' @title Realizar merging das bases
 #'
 #' @return a data frame
 #'
@@ -11,21 +11,8 @@ juntar_bases <- function(base_ibama, base_ibgecode){
   base_merged # salvando csv para realizar testes
 }
 
-id_muni <- function(){
-   ibgecode <-
-    geobr::lookup_muni(code_muni = 'all') %>% # puxando TODOS os municipios
-    dplyr::select(code_muni, name_muni, abrev_state) %>% #selecionando variaveis de interesse
-    dplyr::mutate(name_muni = stringr::str_to_lower(name_muni), # ajustando letras para maiuscula
-    name_muni = abjutils::rm_accent(name_muni), # retirando acentos
-    name_muni = stringr::str_replace_all(name_muni, "-", " "))
-ibgecode
-}
-
-
-
-
 # testando
-base_merged <- juntar_bases(base_ibama = teste, base_ibgecode = ibge)
+# base_neto <- juntar_bases(base_ibama = teste, base_ibgecode = ibge)
 
 # salvando em formato csv
 # write.csv(autuacoes_teste, "data/autuacoes_teste.csv")
