5b87e96ee500c93290fc638d4178cf3a4ec2e548
Claudiano Neto <cneto@ufrb.edu.br>
2021-02-05 10:07:00 -0300

    atualizacao de funcoes

---
+++ R/baixar_id_municipios.R
@@ -0,0 +1,18 @@
+#' @title Baixar codigos municipais do IBGE
+#'
+#' @return a data frame
+#'
+#' @export
+baixar_id_municipios <- function(){
+  ibgecode <-
+    geobr::lookup_muni(code_muni = 'all') %>% # puxando TODOS os municipios
+    dplyr::select(code_muni, name_muni, abrev_state) %>% #selecionando variaveis de interesse
+    dplyr::mutate(name_muni = stringr::str_to_lower(name_muni), # ajustando letras para maiuscula
+                  name_muni = abjutils::rm_accent(name_muni), # retirando acentos
+                  name_muni = stringr::str_replace_all(name_muni, "-", " "))
+  ibgecode
+}
+
+
+
+
