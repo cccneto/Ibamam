233f5ef4ada61658f2c886924963e53b5b4c088d
Beatriz Milz <beatriz.milz@hotmail.com>
2021-05-16 20:07:45 -0300

    funcao raspar_arquivos_arrecadacao vira obter_arrecadacao_estado

--- R/obter_arquivos_arrecadacao.R
+++ R/obter_arquivos_arrecadacao.R
@@ -1,22 +1,61 @@
 #' @title Downloading data from Ibama database - Multas arrecadadas
 #'
-#' @return a data frame
+#' @return A tibble
 #'
 #' @export
 obter_arquivos_arrecadacao <- function() {
-  purrr::map_dfr(
-    unique(geobr::grid_state_correspondence_table$code_state),
-    raspar_arquivos_arrecadacao
+  estados_abrev <- c(
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RS",
+    "RO",
+    "RR",
+    "SC",
+    "SP",
+    "SE",
+    "TO"
   )
+
+
+  purrr::map_dfr(estados_abrev,
+                 obter_arrecadacao_estado)
 
 }
 
-raspar_arquivos_arrecadacao <- function(uf){
+
+#' Obter dados de arrecadacao para um estado
+#'
+#' @param uf Abreviation of the name of the state (two letters in lower case)
+#'
+#' @return A tibble
+#'
+#' @export
+#' @examples obter_arrecadacao_estado("SP")
+obter_arrecadacao_estado <- function(uf) {
   multas_arrecadadas <- jsonlite::fromJSON(
-      paste0(
-        "http://dadosabertos.ibama.gov.br/dados/SICAFI/", uf,
-        "/Arrecadacao/arrecadacaobenstutelados.json"
-      )
+    paste0(
+      "http://dadosabertos.ibama.gov.br/dados/SICAFI/",
+      uf,
+      "/Arrecadacao/arrecadacaobenstutelados.json"
+    )
   )
 
   dplyr::as_tibble(multas_arrecadadas$data)
